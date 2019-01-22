137db85c1f5a64bfd2c1625a5c9d246aeb1c6235
xmile <2243930951@qq.com>
2019-01-22 14:40:49 +0800

    3

--- android/app/src/main/java/processing/test/processing_usbtest2/processing_usbtest2.java
+++ android/app/src/main/java/processing/test/processing_usbtest2/processing_usbtest2.java
@@ -86,6 +86,13 @@ myactivity.openUsbDevice();
       }
       Toast.makeText(this.getActivity(),"open devices successed",Toast.LENGTH_SHORT).show();
       isOpen = true;
+      if (myactivity.usbdriver.SetConfig(9600, (byte)8, (byte)1, (byte)0,(byte)0)) {
+          Toast.makeText(this.getActivity(), "Serial config successed!",
+              Toast.LENGTH_SHORT).show();
+        } else {
+          Toast.makeText(this.getActivity(), "Serial config failed!",
+              Toast.LENGTH_SHORT).show();
+        }
       new readThread().start();
       break;
     default:
@@ -128,7 +135,7 @@ int new_time;
 int old_time=0;
 byte old_s=0;
 public void setup() {
-  System.out.println("fullscreen");
+  System.out.print("fullscreen");
   
   
   ellipseMode(RADIUS);
@@ -152,11 +159,13 @@ public void draw() {
       y=mouseY;
     }
   }
-  //System.out.println(read_recv+"!");
-  text(read_recv+"!",320,1150);
-  String[] t2 = read_recv.split(",");
+  //text(read_recv+"!",320,1150);
+  text("read String:",300,1150);
+  String[] t2 = read_recv.split(" ");
   String t1 = "";
-  for(int i = 0;i<t2.length;i++)if(t2[i].length()>0)t1+=(char)(Integer.parseInt(t2[i],16))+" ";
+  for(int i = 0;i<t2.length;i++){
+    if(t2[i].length()>=2)
+  t1+=(char)(Integer.parseInt(t2[i],16));}
   text(t1+"?",320,1210);
   new_time= millis();
   if((new_time-old_time)>100){
@@ -189,21 +198,23 @@ public void draw() {
   }
 }
 
-
-
-private String toHexString(byte[] arg,int length){
-  String result = "";
-  if(arg != null){
-    for(int i=0;i<length;i++){
-	//System.out.println(arg[i]+"?");
-      result = result+(
-      Integer.toHexString(arg[i]<0?arg[i]+256:arg[i]).length()==1?"0"+Integer.toHexString(arg[i]<0?arg[i]+256:arg[i]):Integer.toHexString(arg[i]<0?arg[i]+256:arg[i])
-      )+",";
+private String toHexString(byte[] arg, int length) {
+    String result = new String();
+    if (arg != null) {
+      for (int i = 0; i < length; i++) {
+        result = result
+            + (Integer.toHexString(
+                arg[i] < 0 ? arg[i] + 256 : arg[i]).length() == 1 ? "0"
+                + Integer.toHexString(arg[i] < 0 ? arg[i] + 256
+                    : arg[i])
+                : Integer.toHexString(arg[i] < 0 ? arg[i] + 256
+                    : arg[i])) + " ";
+      }
+      return result;
     }
-    return result;
+    return "";
   }
-  return "";
-}
+  
 
 
 
